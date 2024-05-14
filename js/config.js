9ab264fc6f27a332847889ffcf33cb109ac43d9f
vivekrekhadia <vivekrekhadia007@gmail.com>
2024-05-14 11:47:52 +0530

    new_config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-south-1_Q1v4y8jec", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "h0qt5afv93hs7mp521mk0pd15", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-south-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
